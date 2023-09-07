dda65e5e8def11cd57c174464c59daf86fd79616
guillaume <48445936+guillaumeFerard@users.noreply.github.com>
2023-09-07 09:46:02 +0200

    Update App.java_crash

--- src/main/java/aqw42/App.java
+++ src/main/java/aqw42/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static String get_message()
     {
-        return "Hello World !";
+        return "Hello Worl!";
     }
 
     public static void main( String[] args )
